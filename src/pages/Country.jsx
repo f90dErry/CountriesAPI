4f5c74fff4bf3654d030a8aa144d9bd97085b4e4
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-02-27 15:35:47 +0000

    country setup

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { IoArrowBack } from 'react-icons/io5'
 
 const Country = () => {
   const [loading, setLoading] = useState(true)
@@ -10,7 +11,6 @@ const Country = () => {
       .then((data) => {
         setCountry(data)
         setLoading(false)
-        console.log(data)
       })
       .catch((error) => {
         console.log('error fetchching data', error)
@@ -18,7 +18,41 @@ const Country = () => {
       })
   }, [])
 
-  return <div></div>
+  return (
+    <div>
+      <button
+        className='flex items-center gap-2 p-2 px-8 my-4 shadow-md'
+        onClick={() => window.history.back()}
+      >
+        <IoArrowBack />
+        Back
+      </button>
+
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {country.map((country, index) => (
+            <div key={index} className=''>
+              <img
+                src={country.flags.png}
+                alt={country.name.common}
+                className='w-32 h-32 object-cover mb-2'
+              />
+              <h2>{country.name.common}</h2>
+              <h3>{country.population}</h3>
+              <h3>{country.region}</h3>
+              <h3>{country.subregion}</h3>
+              <h3>{country.capital}</h3>
+              <h3>{country.tld}</h3>
+              <h3>{country.curencies}</h3>
+              <h3>{country.language}</h3>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Country
