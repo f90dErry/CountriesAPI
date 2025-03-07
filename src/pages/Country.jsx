7dddd758c94ca75061a89d724112e0ffc7842f95
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-03-07 17:13:10 +0000

    ui error fix

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -23,9 +23,9 @@ const Country = () => {
   }, [navigate, params.Countries])
 
   return (
-    <div className='md:mx-24 mx-5'>
+    <div className='md:mx-24 mx-10'>
       <button
-        className='flex items-center gap-2 p-2 px-8 my-4 shadow-sm '
+        className='flex items-center gap-2 p-2 px-8 mt-8 shadow-lg '
         onClick={() => window.history.back()}
       >
         <IoArrowBack />
@@ -39,59 +39,74 @@ const Country = () => {
           {country.map((country, index) => (
             <div key={index}>
               {country.name?.common === params.name && (
-                <div>
-                  <img src={country.flags?.png} alt={country.name?.common} />
-                  <h3 className='text-2xl font-bold md:font-extrabold my-5'>
-                    {country.name?.common}
-                  </h3>
-                  <div className='flex flex-col my-10 gap-2'>
-                    <h4>
-                      <span className='font-medium'>Native Name: </span>
-                      {country.name?.nativeName?.eng?.common}
-                    </h4>
-                    <h4>
-                      <span className='font-medium'>Population: </span>
-                      {country.population
-                        .toString()
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                    </h4>
-                    <h4>
-                      <span className='font-medium'>Region: </span>
-                      {country.region}
-                    </h4>
-                    <h4>
-                      <span className='font-medium'>Sub Region: </span>
-                      {country.subregion}
-                    </h4>
-                    <h4>
-                      <span className='font-medium'>Capital: </span>
-                      {country.capital}
-                    </h4>
-                  </div>
+                <div className='md:flex gap-32 mt-10 items-center justify-center'>
+                  <img
+                    src={country.flags?.png}
+                    alt={country.name?.common}
+                    className='my-10 w-full h-[300px] md:h-[400px] md:w-[600px] object-cover'
+                  />
+                  <div>
+                    <h3 className='text-2xl my-5 font-bold md:font-extrabold '>
+                      {country.name?.common}
+                    </h3>
+                    <div className='md:flex gap-32'>
+                      <div className='flex flex-col mb-8 gap-2'>
+                        <h4>
+                          <span className='font-bold'>Native Name: </span>
+                          {country.name?.nativeName?.eng?.common}
+                        </h4>
+                        <h4>
+                          <span className='font-bold'>Population: </span>
+                          {country.population
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                        </h4>
+                        <h4>
+                          <span className='font-bold'>Region: </span>
+                          {country.region}
+                        </h4>
+                        <h4>
+                          <span className='font-bold'>Sub Region: </span>
+                          {country.subregion}
+                        </h4>
+                        <h4>
+                          <span className='font-bold'>Capital: </span>
+                          {country.capital}
+                        </h4>
+                      </div>
 
-                  <div className='flex flex-col gap-2'>
-                    <h4>
-                      <span className='font-medium'>Top Level Domain: </span>
-                      {country.tld}
-                    </h4>
-                    <h4>
-                      <span className='font-medium'>Currencies: </span>
-                      {country.currencies
-                        ? Object.values(country.currencies)
-                            .map(
-                              (currency) =>
-                                `${currency.name} (${currency.symbol})`
-                            )
-                            .join(', ')
-                        : 'None'}
-                    </h4>
-                    <h4>
-                      <span className='font-medium'>Languages: </span>
-                      {Object.values(country.languages).join(', ')}
-                    </h4>
-                    <h4>
-                      <span className='font-medium'>Border Countries: </span>
-                      {country.borders ? country.borders.join(', ') : 'None'}
+                      <div className='flex flex-col gap-2'>
+                        <h4>
+                          <span className='font-bold'>Top Level Domain: </span>
+                          {country.tld}
+                        </h4>
+                        <h4>
+                          <span className='font-bold'>Currencies: </span>
+                          {country.currencies
+                            ? Object.values(country.currencies)
+                                .map(
+                                  (currency) =>
+                                    `${currency.name} (${currency.symbol})`
+                                )
+                                .join(', ')
+                            : 'None'}
+                        </h4>
+                        <h4>
+                          <span className='font-bold'>Languages: </span>
+                          {Object.values(country.languages).join(', ')}
+                        </h4>
+                      </div>
+                    </div>
+                    <h4 className='my-8 flex flex-col md:flex-row gap-2'>
+                      <span className='font-bold'>Border Countries: </span>
+                      {country.borders.map((border, index) => (
+                        <button
+                          className='bg-white shadow-md px-5 md:px-10 p-1 mx-1'
+                          key={index}
+                        >
+                          {border}
+                        </button>
+                      ))}
                     </h4>
                   </div>
                 </div>
