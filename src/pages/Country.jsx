7b8aebabeab1a4fa606f8df63e7d84f830f8b063
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-03-07 15:53:51 +0000

    single country bug fix

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -23,9 +23,9 @@ const Country = () => {
   }, [navigate, params.Countries])
 
   return (
-    <div>
+    <div className='md:mx-24 mx-5'>
       <button
-        className='flex items-center gap-2 p-2 px-8 my-4 shadow-md'
+        className='flex items-center gap-2 p-2 px-8 my-4 shadow-sm '
         onClick={() => window.history.back()}
       >
         <IoArrowBack />
@@ -35,57 +35,65 @@ const Country = () => {
       {loading ? (
         <h1>Loading</h1>
       ) : (
-        <div>
+        <div className=''>
           {country.map((country, index) => (
             <div key={index}>
               {country.name?.common === params.name && (
                 <div>
                   <img src={country.flags?.png} alt={country.name?.common} />
-                  <h3>{country.name?.common}</h3>
-                  <h4>
-                    <span>Native Name:</span>
-                    {country.name?.nativeName?.eng?.common}
-                  </h4>
-                  <h4>
-                    <span>Population:</span>
-                    {country.population}
-                  </h4>
-                  <h4>
-                    <span>Region:</span>
-                    {country.region}
-                  </h4>
-                  <h4>
-                    <span>Sub Region:</span>
-                    {country.subregion}
-                  </h4>
-                  <h4>
-                    <span>Capital:</span>
-                    {country.capital}
-                  </h4>
-                  <h4>
-                    <span>Top Level Domain:</span>
-                    {country.tld}
-                  </h4>
-                  <h4>
-                    <span>Currencies:</span>
-                    {country.currencies
-                      ? Object.values(country.currencies)
-                          .map(
-                            (currency) =>
-                              `${currency.name} (${currency.symbol})`
-                          )
-                          .join(', ')
-                      : 'None'}
-                  </h4>
+                  <h3 className='text-2xl font-bold md:font-extrabold my-5'>
+                    {country.name?.common}
+                  </h3>
+                  <div className='flex flex-col my-10 gap-2'>
+                    <h4>
+                      <span className='font-medium'>Native Name: </span>
+                      {country.name?.nativeName?.eng?.common}
+                    </h4>
+                    <h4>
+                      <span className='font-medium'>Population: </span>
+                      {country.population
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                    </h4>
+                    <h4>
+                      <span className='font-medium'>Region: </span>
+                      {country.region}
+                    </h4>
+                    <h4>
+                      <span className='font-medium'>Sub Region: </span>
+                      {country.subregion}
+                    </h4>
+                    <h4>
+                      <span className='font-medium'>Capital: </span>
+                      {country.capital}
+                    </h4>
+                  </div>
 
-                  <h4>
-                    <span>Languages:</span>
-                    {Object.values(country.languages).join(', ')}
-                  </h4>
-                  <h4>
-                    <span>Border Countries:</span>
-                    {country.borders ? country.borders.join(', ') : 'None'}
-                  </h4>
+                  <div className='flex flex-col gap-2'>
+                    <h4>
+                      <span className='font-medium'>Top Level Domain: </span>
+                      {country.tld}
+                    </h4>
+                    <h4>
+                      <span className='font-medium'>Currencies: </span>
+                      {country.currencies
+                        ? Object.values(country.currencies)
+                            .map(
+                              (currency) =>
+                                `${currency.name} (${currency.symbol})`
+                            )
+                            .join(', ')
+                        : 'None'}
+                    </h4>
+                    <h4>
+                      <span className='font-medium'>Languages: </span>
+                      {Object.values(country.languages).join(', ')}
+                    </h4>
+                    <h4>
+                      <span className='font-medium'>Border Countries: </span>
+                      {country.borders ? country.borders.join(', ') : 'None'}
+                    </h4>
+                  </div>
                 </div>
               )}
             </div>
